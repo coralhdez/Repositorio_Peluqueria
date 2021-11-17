a13b4abe0755872172b2edfee816fecea1f0d3c6
coralhdez <91058367+coralhdez@users.noreply.github.com>
2021-11-17 17:47:17 +0100

    f
    f

--- Proyecto_Peluqueria/src/main/java/Test/testPeluqueria.java
+++ Proyecto_Peluqueria/src/main/java/Test/testPeluqueria.java
@@ -8,19 +8,24 @@ public class testPeluqueria {
 
     public static void main(String[] args) {
         var nombrefichero = "Peluqueria.txt";
-        //ICatalogoPeliculas catalogo = new CatalogoPeliculasImpl();
         IActividadPeluqueria historialServicios = new ActividadPeluqueriaImpl();
 
+        String nombreServicioAdd = "";
+        double precioAdd = 0.0;
+        int duracionAdd = 0;
+        String descripcionAdd = "";
+        int opcion;
+
         while (true) {
-            System.out.println("MENU");
+            System.out.println("*** MENÚ ***");
             System.out.println(" 1.- Crear Fichero");
             System.out.println(" 2.- Agregar nuevos Servicios");
             System.out.println(" 3.- Visualizar los servicios");
             System.out.println(" 4.- Buscar un servicio");
             System.out.println(" 0.- Salir");
-            System.out.print("Indica la opción deseada: ");
-            Scanner sn = new Scanner(System.in);
-            int opcion = sn.nextInt();
+            System.out.println("Introduzca una opción: ");
+            Scanner ent = new Scanner(System.in);
+            opcion = Integer.parseInt(ent.nextLine());
 
             switch (opcion) {
                 case 1:
@@ -29,17 +34,17 @@ public class testPeluqueria {
                 case 2:
                     System.out.print("Introduce el nombre de la servicio a agregar:\t");
                     Scanner dato = new Scanner(System.in);
-                    String nombreServicioAdd = dato.nextLine();
+                    nombreServicioAdd = dato.nextLine();
 
                     System.out.print("Introduce el precio de la servicio a agregar:\t");
-                    double precioAdd = dato.nextDouble();
+                    precioAdd = dato.nextDouble();
 
                     System.out.print("Introduce la duración del servicio:\t");
-                    int duracionAdd = dato.nextInt();
-                    
+                    duracionAdd = dato.nextInt();
+
                     System.out.println("Introduce la descripción del servicio: ");
-                    String descripcionAdd = dato.nextLine();
-                   
+                    descripcionAdd = dato.nextLine();
+
                     historialServicios.agregarServicio(nombrefichero, new Servicio(nombreServicioAdd, precioAdd, duracionAdd, descripcionAdd));
                     break;
                 case 3:
@@ -52,13 +57,11 @@ public class testPeluqueria {
                     String pBuscar = busqueda.nextLine();
                     historialServicios.buscarUnServicio(nombrefichero, pBuscar);
                     break;
-                case 0:
-                    System.out.println("Gracias!, hasta la proxima");
-                    break;
+                default:
+                    System.out.println("Introduzca un número entre 0 y 5");
 
             }
+            System.out.println();
         }
-
     }
 }
-
